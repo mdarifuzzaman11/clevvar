672d7f13b40a1e5fd9790a67d025ee219de4780a
Md Arifuzzaman <md.arifuzzaman@citytech.cuny.edu>
2022-02-08 16:52:28 -0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Clevvar.com Build Using React.js. This app is under development. happy test
+          Clevvar.com Build Using React.js. This app is under development. happy test, finally it worked
         </p>
         <a
           className="App-link"
